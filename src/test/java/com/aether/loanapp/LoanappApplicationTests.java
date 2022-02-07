36f65219043681ae0acf6c7053db114c128fac4d
Umut Atagun <umutatgn@gmail.com>
2022-02-07 08:45:35 +0300

    Spring.io taslak

---
+++ src/test/java/com/aether/loanapp/LoanappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aether.loanapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
